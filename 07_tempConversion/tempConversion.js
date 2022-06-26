bc04d897d17c6889794b12b86ad42f7960815212
nimishgo <nimish.2711@gmail.com>
2022-06-26 11:59:38 +0530

    Convert temperature solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
+const ftoc = function(F) {
+  
+  let cel = (F - 32) * (5/9);
+
+  return Math.round(cel * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(F) {
+  let Fah = F * (9/5) + 32;
+
+  return Math.round(Fah * 10)/10;
 
 };
 
