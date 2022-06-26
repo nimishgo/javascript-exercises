5eeb476b018a487471cac8abf85c146b9a1c3b35
nimishgo <nimish.2711@gmail.com>
2022-06-26 10:36:05 +0530

    remove from array string passed all test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(...args) {
+
+    let a = args[0];
+    
+    for (const element of args) {
+    // console.log(element);
+    let index = a.indexOf(element);
+
+
+    if(index >= 0 && a[index] === element)
+    {
+    a.splice(index,1);
+    }
+
+    }
+    console.log(a);
+    return a;
 
 };
 
+// removeFromArray(["hey",2,3,"ho"],"hey",3);
+// removeFromArray([1,2,3,4],1,2,3,4);
 // Do not edit below this line
 module.exports = removeFromArray;
