8e30e00b37985620d1722141180202d7e85be6de
nimishgo <nimish.2711@gmail.com>
2022-06-26 10:35:24 +0530

    reverse string passed all test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let len = str.length;
+    let op = "";
+    for (let i = len - 1; i >= 0; i--) {
+        op += str[i]; 
+    }
+    return op;
 };
 
 // Do not edit below this line
