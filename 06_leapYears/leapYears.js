f643fa1ef5362023c67fe37ab688e143143484c0
nimishgo <nimish.2711@gmail.com>
2022-06-26 11:59:19 +0530

    Check leap years solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
 
+    if(leapYear % 4 === 0 && leapYear%100 !== 0) return true;
+
+    else if ( leapYear % 400 === 0) return true;
+
+    else return false;
 };
 
+// console.log(leapYears(1800));
+// console.log(leapYears(1900));
+// console.log(leapYears(2000));
+// console.log(leapYears(1600));
 // Do not edit below this line
 module.exports = leapYears;
